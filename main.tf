f18db8ec0e5523d7262bf242670375b8bbec1674
sreemanos <74346692+sreemanos@users.noreply.github.com>
2022-01-13 19:46:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "Aspire_personal"
+    organization = " Aspire_personal"
 
     workspaces {
       name = "gh-actions-demo"
